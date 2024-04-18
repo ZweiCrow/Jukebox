4fee40f7d29fdd1d5bda848ea55b04d96af52041
ZweiCrow <squall.flamme@gmail.com>
2024-04-18 11:04:55 +0200

    v0.4 - Page Albums WIP / listing and serach bar done, navigation todo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import Player from "./Components/Player";
+import Albums from "./Components/Albums";
 
 function App() {
   return (
     <Routes>
       <Route path="/" Component={Home}/>
-      <Route path="/Albums" Component={Home}/>
+      <Route path="/Albums" Component={Albums}/>
       <Route path="/Artists" Component={Home}/>
       <Route path="/SoundTracks" Component={Home}/>
       <Route path="/Play" Component={Player}/>
